e4b880b0351840d081a172b14f0283ae820f196a
Suprem Vanam <supremvanam.gsp@gmail.com>
2022-05-20 13:27:24 -0400

    Addead MealSummary.js

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import { Fragment } from "react";
+import styles from "./Header.module.css";
+import mealsImg from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+
+export default function Header(props) {
+  return (
+    <Fragment>
+      <header className={styles.header}>
+        <h1>Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={styles["main-image"]}>
+        <img src={mealsImg} alt="A table full of delicious food" />
+      </div>
+    </Fragment>
+  );
+}
