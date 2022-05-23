e812dd9259ee1b38c946dc83b8058ac5d87f274f
Suprem Vanam <supremvanam.gsp@gmail.com>
2022-05-23 12:21:22 -0400

    Add Header Cart Button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,23 @@
 import React from "react";
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
+import { useContext } from "react";
+import CartContext from "../../context/cart-context";
 
 export default function HeaderCartButton(props) {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((currentNum, item) => {
+    return currentNum + item.totalAmount;
+  }, 0);
+
   return (
     <button className={styles.button} onClick={props.cartClickHandler}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Cart</span>
-      <span className={styles.badge}>{2 + 1}</span>
+      <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
   );
 }
