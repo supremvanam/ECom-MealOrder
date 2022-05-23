adccfbe4c9e3aa5210b5f3d0ba96a97bd14759c0
Suprem Vanam <supremvanam.gsp@gmail.com>
2022-05-23 12:19:35 -0400

    Configure Context Provider for Cart

--- src/context/CartProvider.js
+++ src/context/CartProvider.js
@@ -1,12 +1,47 @@
 import CartContext from "./cart-context";
+import { useReducer } from "react";
+
+const ACTIONS = {
+  ADD_ITEM: "ADD",
+  REMOVE_ITEM: "REMOVE",
+};
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.ADD_ITEM:
+      const updatedItems = state.items.concat(action.payload.item);
+      const updatedTotalAmount = state.totalAmount + action.payload.item.amount;
+      return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount,
+      };
+
+    case ACTIONS.REMOVE_ITEM:
+      return;
+  }
+
+  return defaultCartState;
+};
 
 const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: ACTIONS.ADD_ITEM, payload: { item: item } });
+  };
   const removeItemFromCartHandler = (id) => {};
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
